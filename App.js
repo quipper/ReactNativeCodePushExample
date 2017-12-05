c065e64f91e853890bfd1be4ce3a79af919a3c7e
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2017-12-06 05:01:33 +0900

    Link to open the recruitment page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Linking, Image } from 'react-native';
 import codePush from 'react-native-code-push';
 
 const styles = StyleSheet.create({
@@ -9,11 +9,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inner: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    marginTop: 10,
+    fontSize: 20,
+  }
 });
+
+const onPress = () => {
+  Linking.openURL('https://www.quipper.com/career/');
+};
 
 const App = () => (
   <View style={styles.container}>
-    <Text>Quipper is hiring React Native developers!</Text>
+    <TouchableHighlight onPress={() => onPress()}>
+      <View style={styles.inner}>
+        <Image source={require('./logo.png')} />
+        <Text style={styles.text}>Quipper is hiring React Native developers!</Text>
+      </View>
+    </TouchableHighlight>
   </View>
 );
 
